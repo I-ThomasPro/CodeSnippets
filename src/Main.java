114d2d82ea068b7ce4d3908ae8eb087d1d622a39
i-thomaspro <ipro2@huskers.unl.edu>
2023-01-08 20:22:44 -0600

    Added a utils class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        System.out.println(Utils.add(4,1));
     }
 }
---
+++ src/Utils.java
@@ -0,0 +1,7 @@
+public class Utils
+{
+    public static int add(int x, int y)
+    {
+        return x+y;
+    }
+}
