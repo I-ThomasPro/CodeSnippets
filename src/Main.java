5771955df30b4b07b173d65376e7ae18032a721e
i-thomaspro <ipro2@huskers.unl.edu>
2023-01-08 20:47:37 -0600

    Playng around

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,15 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println(Utils.add(4,1));
+        Utils.print_one_through_ten();
+
+        try
+        {
+            Utils.divide_by_zero();
+        }
+        catch (ArithmeticException ae)
+        {
+            System.err.println("Can't divide by zero.");
+        }
     }
 }
--- src/Utils.java
+++ src/Utils.java
@@ -1,7 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Utils
 {
     public static int add(int x, int y)
     {
         return x+y;
+    }
+
+    public static void print_one_through_ten()
+    {
+        List<Integer> numbers = new ArrayList<>();
+        for(int i=1; i<=10; i++)
+            numbers.add(i);
+        for(int i : numbers)
+            System.out.println(i);
+    }
+
+    public static void divide_by_zero()
+    {
+        double x = 5 / 0;
     }
 }
